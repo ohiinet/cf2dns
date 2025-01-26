959d139382fe28cf470eacf4f7a0964abc3da5e1
ohiinet <104287973+ohiinet@users.noreply.github.com>
2025-01-26 13:49:47 +0800

    Update cf2dns_actions.py

--- cf2dns_actions.py
+++ cf2dns_actions.py
@@ -22,7 +22,7 @@ SECRETKEY = os.environ["SECRETKEY"]   #'ZrVs*************gqjOp1zVl'
 #DNSPOD免费版只支持两条, 华为支持集合
 AFFECT_NUM = 1
 #DNS服务商 DNSPod改为1 阿里云解析改成2  华为云解析改成3 华为云解析集合3.1
-DNS_SERVER = 3
+DNS_SERVER = 3.1
 #如果试用华为云解析 需要从API凭证-项目列表中获取
 REGION_HW = 'cn-east-3'
 #如果使用阿里云解析 REGION出现错误再修改 默认不需要修改 https://help.aliyun.com/document_detail/198326.html
