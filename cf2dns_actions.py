987acb3a682075e396d7190e4c02fddd46e5fdc2
ohiinet <104287973+ohiinet@users.noreply.github.com>
2025-01-26 21:51:18 +0800

    Update cf2dns_actions.py

--- cf2dns_actions.py
+++ cf2dns_actions.py
@@ -113,7 +113,7 @@ def changeDNS(line, s_info, c_info, domain, sub_domain, cloud):
                         continue
                 ret = cloud.change_record(domain, info["recordId"], sub_domain, cf_ip, RECORD_TYPE, line, TTL)
                 if(DNS_SERVER != 1 or ret["code"] == 0):
-                    print("CHANGE DNS SUCCESS: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " you momain "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+line+"----RECORDID: " + str(info["recordId"]) + "----VALUE: " + str(cf_ip) )
+                    print("CHANGE DNS SUCCESS")
                 else:
                     print("CHANGE DNS ERROR: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " + domain + "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+line+"----RECORDID: " + str(info["recordId"]) + "----VALUE: " + str(cf_ip) + "----MESSAGE: " + ret["message"] )
         elif create_num > 0:
